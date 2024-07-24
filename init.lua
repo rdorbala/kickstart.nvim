0e03d7b5d6cb907f8f0cf00165a3e303a0f7a7de
Crane <elysiano@live.com>
2024-07-23 22:17:46 -0700

    add line numbers

--- init.lua
+++ init.lua
@@ -100,6 +100,7 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 -- vim.opt.number = true
+vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
